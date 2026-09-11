0f3337d5c20e3f30f4421a467fed9b3eca1f6af8
lilseniorj <jesusvarguer18@gmail.com>
2026-09-11 18:26:48 -0500

    Eliminar notas con Flask y SQLAlchemy

--- app.py
+++ app.py
@@ -101,3 +101,10 @@ def edit_note(note_id):
 
     return render_template("edit_note.html", note=note)
 
+
+@app.route("/delete-note/<int:note_id>", methods=["POST"])
+def delete_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for("home"))
